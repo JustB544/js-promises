5b3cf727b9a0ff904b0d80f87d8d33df26920cc7
Breyton Pabst <breytonpabst@gmail.com>
2023-09-28 23:02:00 -0600

    finished part 2

--- app.js
+++ app.js
@@ -10,68 +10,53 @@ const num_api = "http://numbersapi.com";
 let deck = "";
 
 
-function num_api_1(){
-    const promise = axios.get(`${num_api}/${nums}?json`)
-    .then(({data}) => {
-        for (let key in data){
-            const el = document.createElement("span");
-            el.innerText = key;
-            body.appendChild(el);
-            body.appendChild(document.createElement("br"));
-        }
-    })
-    .catch(() => console.log("Error"));
+async function num_api_1(){
+    const {data} = await axios.get(`${num_api}/${nums}?json`);
+    for (let key in data){
+        const el = document.createElement("span");
+        el.innerText = key;
+        body.appendChild(el);
+        body.appendChild(document.createElement("br"));
+    }
 }
 
-function num_api_2(){
+async function num_api_2(){
     const promises = [];
     for (let i = 0; i < 4; i++){
         promises.push(axios.get(`${num_api}/${fav_num}?json`));
     }
-    Promise.all(promises)
-    .then((data) => {
-        data.forEach((fact) => {
-            const el = document.createElement("span");
-            el.innerText = fact.data.text;
-            body.appendChild(el);
-            body.appendChild(document.createElement("br"));
-        });
-    })
-    .catch(() => console.log("Error"));
+    const responses = await Promise.all(promises);
+    responses.forEach((fact) => {
+        const el = document.createElement("span");
+        el.innerText = fact.data.text;
+        body.appendChild(el);
+        body.appendChild(document.createElement("br"));
+    });
 }
 
-function get_new_deck(){
+async function get_new_deck(){
     if (localStorage.getItem("deck") === null){
-        const deck = axios.get("https://deckofcardsapi.com/api/deck/new/shuffle")
-        .then(({data}) => {
-            console.log(data)
-            localStorage.setItem("deck", data.deck_id);
-        })
-        .catch(() => console.log("Error"));
+        const {data} = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle")
+        localStorage.setItem("deck", data.deck_id);
     }
     else {
         deck = localStorage.getItem("deck");
-        axios.get(`https://deckofcardsapi.com/api/deck/${deck}/shuffle`)
-        .catch(() => {
+        const {data} = await axios.get(`https://deckofcardsapi.com/api/deck/${deck}/shuffle`)
+        if (data.success === false){
             localStorage.removeItem("deck");
             get_new_deck();
-        });
+        }
     }
 }
 
 function cards_api(){
     get_new_deck();
     button.hidden = false;
-    button.addEventListener("click", () => {
-        const promise = axios.get(`https://deckofcardsapi.com/api/deck/${deck}/draw/`)
-        .then(({data}) => {
-            console.log(data);
-            img.src = data.cards["0"].image;
-            console.log(data.remaining);
-            if (data.remaining === 0){
-                axios.get(`https://deckofcardsapi.com/api/deck/${deck}/shuffle`);
-            }
-        })
-        .catch(() => console.log("Error"));
+    button.addEventListener("click", async () => {
+        const {data} = await axios.get(`https://deckofcardsapi.com/api/deck/${deck}/draw/`)
+        img.src = data.cards["0"].image;
+        if (data.remaining === 0){
+            axios.get(`https://deckofcardsapi.com/api/deck/${deck}/shuffle`);
+        }
     });
 }
